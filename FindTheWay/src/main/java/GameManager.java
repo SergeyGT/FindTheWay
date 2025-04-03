5b3a72943f469ecd1232b25dcee3ee57f43ab5e2
Burdenyuk Sergey <79414642+SergeyGT@users.noreply.github.com>
2025-04-04 00:43:28 +0300

    Добавлены необходимые классы и библиотеки

---
+++ FindTheWay/src/main/java/GameManager.java
@@ -0,0 +1,20 @@
+import lombok.Data;
+
+@Data
+public class GameManager {
+    private GameField _field;
+    private Maze _maze;
+    private boolean _gameStatus;
+
+    private void StartGame(){
+
+    }
+
+    private void EndGame(){
+
+    }
+
+    private void CreateField(){
+        
+    }
+}
